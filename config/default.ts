3fbc4d0b3927335f1a1db8de1da911633ad19238
Caephas <arinzeobidiegwu@gmail.com>
2022-07-21 13:20:59 +0100

    removed a folder

--- config/default.ts
+++
@@ -1,6 +0,0 @@
-export default{
-    // TODO:: change port in postman
-    port: 7000,
-    dbUri: "mongodb+srv://Caephas:Ephata35923@cluster0.s3jcz.mongodb.net/?retryWrites=true&w=majority",
-    saltWorkFactor: 10,
-}
