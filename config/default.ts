4f219a0c8fc8197dcde8b39e6a2377321430a2ac
Caephas <arinzeobidiegwu@gmail.com>
2022-07-21 13:09:20 +0100

    user creation successful

---
+++ config/default.ts
@@ -0,0 +1,6 @@
+export default{
+    // TODO:: change port in postman
+    port: 7000,
+    dbUri: "mongodb+srv://Caephas:Ephata35923@cluster0.s3jcz.mongodb.net/?retryWrites=true&w=majority",
+    saltWorkFactor: 10,
+}
